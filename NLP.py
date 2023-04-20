cdb13d98c2b3607cdcc0e9b27bc4539565bc6b26
AkinoriKoerich <yuukko404@gmail.com>
2023-04-20 04:29:25 -0300

    Update Inputs

--- NLP.py
+++ NLP.py
@@ -12,7 +12,7 @@ from sklearn.metrics import accuracy_score
 import pickle
 
 # Import DataSet from Pandas
-data = pd.read_csv(r'dataset/IMDB-Dataset.csv')
+data = pd.read_csv('IMDB-Dataset.csv')
 
 
 # Remove HTML Tags and other with Regex rule
@@ -37,9 +37,6 @@ def to_lower(text):
     return text.lower()
 
 
-nltk.download('stopwords')
-nltk.download('punkt')
-
 def rem_stopwords(text):
     stop_words = set(stopwords.words('english'))
     words = word_tokenize(text)
@@ -57,15 +54,11 @@ X = np.array(data.iloc[:,0].values)
 y = np.array(data.sentiment.values)
 cv = CountVectorizer(max_features = 1000)
 X = cv.fit_transform(data.review).toarray()
-print("X.shape = ",X.shape)
-print("y.shape = ",y.shape)
 
 
 # Train test split (ML)
 
 trainx, testx, trainy, testy = train_test_split(X,y,test_size=0.2, random_state=9)
-print('Train shapes : X = {}, y = {}'.format(trainx.shape, trainy.shape))
-print('Test shapes : X = {}, y = {}'.format(testx.shape, testy.shape))
 
 # Defining the models and Training them
 
@@ -80,13 +73,30 @@ ypg = gnb.predict(testx)
 ypm = gnb.predict(testx)
 ypb = gnb.predict(testx)
 
-print('Gaussian = ', accuracy_score(testy,ypg))
-print('Multinomial = ',accuracy_score(testy,ypm))
-print('Bernoulli = ',accuracy_score(testy,ypb))
-
 pickle.dump(bnb,open('modell.pkl', 'wb'))
 
 
-# Utilizing the predictive machine for analise of things
+# Utilizing inputs
 
+rev = 'A wonderful little production. The filming technique is very unassuming- very old-time-BBC fashion and gives a comforting, and sometimes discomforting, sense of realism to the entire piece. The actors are extremely well chosen- Michael Sheen not only "has got all the polari" but he has all the voices down pat too! You can truly see the seamless editing guided by the references to Williams diary entries, not only is it well worth the watching but it is a terrificly written and performed piece. A masterful production about one of the great masters of comedy and his life. The realism really comes home with the little things: the fantasy of the guard which, rather than use the traditional dream techniques remains solid then disappears. It plays on our knowledge and our senses, particularly with the scenes concerning Orton and Halliwell and the sets particularly of their flat with Halliwells murals decorating every surface are terribly well done.'
 
+f1 = clean(rev)
+f2 = is_special(f1)
+f3 = to_lower(f2)
+f4 = rem_stopwords(f3)
+f5 = stem_txt(f4)
+
+bow,words = [], word_tokenize(f5)
+
+for word in words:
+    bow.append(words.count(word))
+
+word_dict = cv.vocabulary_
+pickle.dump(word_dict, open('bow.pkl', 'wb'))
+
+inp = []
+for i in word_dict:
+    inp.append(f5.count(i[0]))
+y_pred = bnb.predict(np.array(inp).reshape(1, 1000))
+
+print(y_pred)
